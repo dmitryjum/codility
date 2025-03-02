ffb8eb7acd6f6ccf03638b12e49b7197ea5be1ff
Dmitry Jum <xfiler@ymail.com>
2025-03-02 03:44:12 -0800

    fixes frog-river-one solution

--- frog_river_one/solution.rb
+++ frog_river_one/solution.rb
@@ -1,8 +1,17 @@
 require 'pry'
 
 def solution(x, a)
-  result = a.index(x)
-  result = result.nil? ? -1 : result
+  seen = Array.new(x + 1, false)
+  count = 0
+  a.each_with_index do |stone, time|
+    if !seen[stone] && count <= x
+      seen[stone] = true
+      count += 1
+      return time if count == x
+    end
+  end
+  -1
 end
 
+puts solution(5, [1,3,1,4,2,3,5,4])
 puts solution(2, [2, 2, 2, 2, 2])
