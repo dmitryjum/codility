1798cdb81860f2a29c1d5e6461ec99394cdcad9a
Dmitry Jum <xfiler@ymail.com>
2025-03-08 02:49:09 -0800

    WIP: adds solution to genomic_range_query but O(n^2), needs performance improvement

--- genomic_range_query/solution.rb
+++ genomic_range_query/solution.rb
@@ -1,1 +1,26 @@
-solution.rb
+require 'pry'
+
+def solution(s, p, q)
+  impact_factors = {
+    "A": 1,
+    "C": 2,
+    "G": 3,
+    "T": 4
+  }
+
+  result = []
+
+  p.each_with_index do |num, idx|
+    smallest = Float::INFINITY
+    (num..q[idx]).each do |i|
+      smallest = impact_factors[s[i].to_sym] if impact_factors[s[i].to_sym] < smallest
+    end
+    result << smallest
+  end
+  result
+end
+
+s = "CAGCCTA"
+first = [2,5,0]
+second = [4,5,6]
+puts solution(s, first, second)
