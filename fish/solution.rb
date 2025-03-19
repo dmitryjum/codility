c57df5cce49018760607ae9460e2cecf3705385c
Dmitry Jum <xfiler@ymail.com>
2025-03-18 21:35:45 -0700

    improves fish solution

--- fish/solution.rb
+++ fish/solution.rb
@@ -1,27 +1,24 @@
 require 'pry'
 
 def solution(a, b)
-  fish_stack = [[a.first, b.first]]
-  (0...a.length - 2).each do |i|
-    if fish_stack.last.last > b[i + 1]
-      if fish_stack.last.first < a[i + 1]
-        fish_stack.pop
-        # call the solving function again recursively here
-        # get rid of the loop and recursively solve this
-        fish_stack << [a[i + 1], b[i + 1]]
-      else
-        a.delete_at(i + 1)
-        b.delete_at(i + 1)
-      end
-    else
-      fish_stack << [a[i + 1], b[i + 1]]
+  downstream = []
+  survivors = 0
+  a.each_with_index do |fish, i|
+    downstream << fish if b[i] == 1
+    if b[i] == 0
+      downstream.pop while downstream.size > 0 && downstream.last < fish #upstream fish eats the downstream fish
+      survivors += 1 if downstream.empty? #fish is going upstream with no collision with downstream fish
     end
   end
-  fish_stack.length
+  downstream.size + survivors
 end
 
 a = [4,3,2,1,5]
 b = [0,1,0,0,0]
 c = [4,3,2,1,5]
-d = [0,1,0,1,0] # 4 3 5
-puts solution(a, b)
+d = [0,1,0,1,0]
+e = [0,1]
+f = [1,1]
+puts solution(c, d)
+puts solution(e,f)
+puts solution(a,b)
