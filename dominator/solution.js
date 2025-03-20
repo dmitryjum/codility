8e8805d772d3e946220e9211cc0cb64b3e6fb670
Dmitry Jum <xfiler@ymail.com>
2025-03-19 17:24:33 -0700

    refactors dominator solution slightly

--- dominator/solution.js
+++ dominator/solution.js
@@ -1,12 +1,12 @@
 function solution(A) {
   const halfLen = A.length / 2
   let nums = {}
-  let dominator;
+  let dominator = -1;
   A.forEach((n, i) => {
     nums[n] === undefined ? nums[n] = 1 : nums[n]++
     if (nums[n] > halfLen) dominator = i
   });
-  return dominator === undefined ? -1 : dominator
+  return dominator
 }
 
 console.log(solution([3,4,3,2,3,-1,3,3]));
