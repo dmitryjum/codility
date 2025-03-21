6e8c0480c6bb80aea74da192f50498685e3efd7d
Dmitry Jum <xfiler@ymail.com>
2025-03-20 20:48:39 -0700

    improved max profit solution from O(n^2) to O(n)

--- max_profit/solution.rb
+++ max_profit/solution.rb
@@ -2,10 +2,10 @@ require 'pry'
 
 def solution(a)
   max_profit = 0
-  cal_length = a.length
-  a[0...cal_length -1].each_with_index do |price, day|
-    best_sell_price = a[day + 1...cal_length].max
-    cycle_profit = best_sell_price - price
+  best_buy_price = Float::INFINITY
+  a[0...a.length-1].each_with_index do |price, day|
+    best_buy_price = price if price < best_buy_price
+    cycle_profit = a[day + 1] - best_buy_price
     max_profit = cycle_profit if max_profit < cycle_profit
   end
   max_profit
